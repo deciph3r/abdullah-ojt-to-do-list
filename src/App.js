a8d90273e78e444572be700f10dc8ca4326dfaff
Ahamed Abdullah <ahamed.abdullah00772@gmail.com>
2021-06-16 19:07:10 +0530

    simple todolist

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import { useState } from "react";
+
+
+function TaskList({ task, setTask }) {
+  const temp = [];
+  const onClickHandler = (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      const t = task.filter((elemet) => elemet !== e.target.previousElementSibling.innerHTML)
+      setTask(t)
+    }
+  }
+  for (let i = 0; i < task.length; i++) {
+    temp.push(
+      <div key={i} id={`${i}`} onClick={onClickHandler} className=" bg-gray-700 p-5 rounded-md mb-3">
+        <span className="text-white">{task[i]}</span>
+        <button className="mx-3 bg-white p-3 rounded-md">Delete</button>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      {temp}
+    </>
+  )
+}
+
 function App() {
+
+  const [task, setTask] = useState([]);
+  const [inputTask, setInputTask] = useState('');
+
   return (
     <div className="App ">
-      <h1>Hello World</h1>
+      <div className="flex justify-center pt-10 mb-3">
+        <input type="text" placeholder="Enter Task" className="" value={inputTask} onChange={(e) => setInputTask(e.target.value)} />
+        <button className="bg-white mx-2 p-3" onClick={() => { setTask((task) => [...task, `${inputTask}`]); setInputTask('') }}>Add</button>
+      </div>
+
+      <div className="flex flex-col justify-center ml-3">
+        <TaskList task={task} setTask={setTask} />
+      </div>
     </div>
   );
 }
